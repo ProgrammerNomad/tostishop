c5b1e35886f6c073de378f7775df39c7aa77adfc
Shiv Singh <shiv@srapsware.com>
2025-08-08 16:16:07 +0530

    Enhance product rating display to show "No reviews yet" for unrated products

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -739,26 +739,28 @@ if ($product && is_a($product, 'WC_Product') && method_exists($product, 'get_nam
                 </header>
                 
                 <!-- Rating -->
-                <?php if ($product->get_average_rating()) : ?>
-                    <div class="flex items-center space-x-2">
-                        <div class="flex text-yellow-400">
-                            <?php
-                            $rating = $product->get_average_rating();
-                            for ($i = 1; $i <= 5; $i++) :
-                                if ($i <= $rating) : ?>
-                                    <svg class="w-5 h-5 fill-current" viewBox="0 0 20 20">
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                                    </svg>
-                                <?php else : ?>
-                                    <svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 20 20">
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                                    </svg>
-                                <?php endif;
-                            endfor; ?>
-                        </div>
-                        <span class="text-sm text-gray-600"><?php echo $product->get_review_count(); ?> <?php _e('reviews', 'tostishop'); ?></span>
+                <div class="flex items-center space-x-2 mb-4">
+                    <div class="flex text-yellow-400">
+                        <?php
+                        $rating = $product->get_average_rating();
+                        for ($i = 1; $i <= 5; $i++) :
+                            if ($rating > 0 && $i <= $rating) : ?>
+                                <svg class="w-5 h-5 fill-current" viewBox="0 0 20 20">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                                </svg>
+                            <?php else : ?>
+                                <svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 20 20">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                                </svg>
+                            <?php endif;
+                        endfor; ?>
                     </div>
-                <?php endif; ?>
+                    <?php if ($rating > 0) : ?>
+                        <span class="text-sm text-gray-600"><?php echo $product->get_review_count(); ?> <?php echo _n('review', 'reviews', $product->get_review_count(), 'tostishop'); ?></span>
+                    <?php else : ?>
+                        <span class="text-sm text-gray-400"><?php _e('No reviews yet', 'tostishop'); ?></span>
+                    <?php endif; ?>
+                </div>
                 
                 <!-- Price -->
                 <div class="flex items-center space-x-3 mb-4">
